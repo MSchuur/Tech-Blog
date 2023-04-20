816c1cbceb7310e0d000d11241a92b9d60cefb47
MSchuur <mschuur31@gmail.com>
2023-04-20 13:39:35 -0300

    Populated the DB/schema.sql file, Populated the connection.js file and reorganized the server.js file.

--- server.js
+++ server.js
@@ -1,15 +1,19 @@
 /* eslint-disable no-undef */
-const express = require('express');
-const routes = require('./controllers');
-const sequelize = require('./config/connection');
 const path = require('path');
-const helpers = require('./utils/helpers');
-const exphbs = require('express-handlebars');
-const hbs = exphbs.create({ helpers});
+const express = require('express');
 const session = require('express-session');
+const exphbs = require('express-handlebars');
+const routes = require('./controllers');
+const helpers = require('./utils/helpers');
+
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
 const app = express();
 const PORT = process.env.PORT || 3001;
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+// Create the Handlebars.js engine object with custom helper functions
+const hbs = exphbs.create({ helpers});
 
 const sess = {
 	secret: 'Super secret secret',
@@ -22,11 +26,15 @@ const sess = {
 };
 
 app.use(session(sess));
-app.use(express.json());
-app.use(express.urlencoded({ extend: true }));
-app.use(express.static(path.join(__dirname, 'public')));
+
+// Inform Express.js which template engine we're using
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
